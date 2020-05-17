b5093ac51f9b779cf1f52766f8abec146b5a345d
helmuthrosales <49173533+helmuthrosales@users.noreply.github.com>
2020-05-17 15:15:33 -0400

    Add files via upload

--- code/rf95_server_rssiLED.ino
+++ code/rf95_server_rssiLED.ino
@@ -1,3 +1,8 @@
+/*Code below is adapted from the "rf95_server" example from the Radiohead library (https://www.airspayce.com/mikem/arduino/RadioHead/index.html)
+  This sketch uses the Radiohead library to configure a LoRa module as a server, and then reads the RSSI from incoming messages to set
+  an LED color, effectively using RSSI as a proxy for proximity sensing.
+*/
+
 // rf95_server.pde
 // -*- mode: C++ -*-
 /* Example sketch showing how to create a simple messaging server with the RH_RF95 class.
@@ -5,9 +10,8 @@
   It is designed to work with the other example rf95_client
 */
 
-
 #include <SPI.h>
-#include <RH_RF95.h> //http://www.airspayce.com/mikem/arduino/RadioHead/classRH__RF95.html#details
+#include <RH_RF95.h>
 
 // Singleton instance of the radio driver
 RH_RF95 rf95;
@@ -17,7 +21,8 @@ RH_RF95 rf95;
 #define LED_COUNT 1
 #define LED_PIN 6
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-//rssi values found through testing. Rssi closer to 0 is stronger.
+
+//Set range of RSSI values. These values found through testing
 int rssiStrongest = -20;
 int rssiWeakest = -130;
 
@@ -28,17 +33,6 @@ void setup()
   while (!Serial) ; // Wait for serial port to be available
   if (!rf95.init())//
     Serial.println("init failed");
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-
-  // The default transmitter power is 13dBm, using PA_BOOST.
-  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
-  // you can set transmitter powers from 5 to 23 dBm:
-  //  driver.setTxPower(23, false);
-  // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
-  // transmitter RFO pins and not the PA_BOOST pins
-  // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
-  // Failure to do that will result in extremely low transmit powers.
-  //  driver.setTxPower(14, true);
 
   //Initiate LED
   pinMode(LED_PIN, OUTPUT);
@@ -53,29 +47,10 @@ void loop()
   {
     // there should be a message incoming
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-    /*  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-      uint8_t is the same as a byte. A byte stores an 8-bit unsigned number, from 0 to 255
-      "buf[x]" creates an array of length within brackets
-      If I use a lower value than the max length possible under this freq,
-      then it will just truncate the message
-    */
     uint8_t len = sizeof(buf);
-    /*  uint8_t len = sizeof(buf);
-      "get number of bytes occupied by the array"
-      The sizeof operator returns the number of bytes in a variable type.
-    */
     if (rf95.recv(buf, &len))
-      /*recv() Turns the receiver on if it not already on.
-        2 arguments
-          -x: Location to copy received message (in the array we set at start of loop)
-          -y: Pointer to available space in buf. Set to actual number of octets copied
-         If there's a valid message available, copy it to buf and return true else return false.
-         "&" is a "Pointer Access Operator"
-         "&x" represents the memory address of the variable.
-      */
     {
-
-      //      RH_RF95::printBuffer("request: ", buf, len);
+      RH_RF95::printBuffer("request: ", buf, len);
       Serial.print("Client Says: ");
       Serial.println((char*)buf);
       Serial.print("RSSI: ");
@@ -86,20 +61,17 @@ void loop()
       int redledVal = constrain(redLightMap, 0, 255);
       Serial.print("redledVal:");
       Serial.println(redledVal);
+
       int greenLightMap = map(rf95.lastRssi(), rssiWeakest, rssiStrongest, 255, 0);
       int greenledVal = constrain(greenLightMap, 255, 0);
       Serial.print("greenledVal:");
       Serial.println(greenledVal);
+
       strip.setPixelColor(0, redledVal, greenledVal, 0);
       strip.show();
 
-      /*
-         char prints characters stored in buf.
-         " * " points to the array stores in buf
-      */
-
       // Send a reply
-      uint8_t data[] = "Hi, I'm the server!";//array called data of size set by reply
+      uint8_t data[] = "Hi, I'm the server!";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
       Serial.println("Sent a reply");
@@ -108,9 +80,6 @@ void loop()
     else
     {
       Serial.println("recv failed");
-      //set led blue if server not running
-      strip.setPixelColor(0, 0, 0, 255);
-      strip.show();
     }
   }
 }
