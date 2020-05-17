44a783002a055bdf3219aa3361bbededb8848d86
helmuthrosales <49173533+helmuthrosales@users.noreply.github.com>
2020-05-17 15:17:28 -0400

    Configures LoRa client with led indicating RSSI

--- code/rf95_client_rssiLED.ino
+++ code/rf95_client_rssiLED.ino
@@ -1,3 +1,8 @@
+/*Code below is adapted from the "rf95_client" example from the Radiohead library (https://www.airspayce.com/mikem/arduino/RadioHead/index.html)
+  This sketch uses the Radiohead library to configure a LoRa module as a client, and then reads the RSSI from incoming messages to set
+  an LED color, effectively using RSSI as a proxy for proximity sensing.
+*/
+
 // rf95_client.pde
 // -*- mode: C++ -*-
 /* Example sketch showing how to create a simple messageing client
@@ -7,19 +12,18 @@
   It is designed to work with the other example rf95_server
 */
 
-
 #include <SPI.h>
 #include <RH_RF95.h>
 
-//LED SETUP
+//LED setup
 #include <Adafruit_NeoPixel.h>
 #define LED_COUNT 1
 #define LED_PIN    6
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-//rssi values found through testing. Rssi closer to 0 is stronger.
+
+//Set range of RSSI values. These values found through testing
 int rssiStrongest = -20;
 int rssiWeakest = -130;
-
 
 // Singleton instance of the radio driver
 RH_RF95 rf95;
@@ -30,8 +34,6 @@ void setup()
   while (!Serial) ; // Wait for serial port to be available
   if (!rf95.init())
     Serial.println("init failed");
-  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
-  // The default transmitter power is 13dBm, using PA_BOOST.
 
   //Initiate LED
   pinMode(LED_PIN, OUTPUT);
@@ -43,11 +45,9 @@ void setup()
 
 void loop()
 
-//calibrate distance
-
 {
-  //  Serial.println("Sending to rf95_server");
-  // Send a message to rf95_server
+  Serial.println("Sending to rf95_server");
+  //  Send a message to rf95_server
   uint8_t data[] = "Hello, I'm the client";
   rf95.send(data, sizeof(data));
 
@@ -61,20 +61,25 @@ void loop()
     // Should be a reply message for us now
     if (rf95.recv(buf, &len))
     {
-      //      Serial.print("Server says: ");
-      //      Serial.println((char*)buf)
+      Serial.print("Server says: ");
+      Serial.println((char*)buf);
       Serial.print("RSSI:");
       Serial.println(rf95.lastRssi(), DEC);
 
-      //map rssi to color range
+      //map rssi to color range.
+
+      // red led (255,0,0) will mean weak signal
       int redLightMap = map(rf95.lastRssi(), rssiWeakest, rssiStrongest, 255, 0);
       int redledVal = constrain(redLightMap, 0, 255);
       Serial.print("redledVal:");
       Serial.println(redledVal);
+
+      // green led (0,255,0) will mean strong signal
       int greenLightMap = map(rf95.lastRssi(), rssiWeakest, rssiStrongest, 255, 0);
       int greenledVal = constrain(greenLightMap, 255, 0);
       Serial.print("greenledVal:");
       Serial.println(greenledVal);
+
       strip.setPixelColor(0, redledVal, greenledVal, 0);
       strip.show();
     }
@@ -87,7 +92,7 @@ void loop()
   {
 
     Serial.println("No reply, is rf95_server running?");
-    //set led blue if server not running
+    //set led blue if no signal recieved from server
     strip.setPixelColor(0, 0, 0, 255);
     strip.show();
   }
